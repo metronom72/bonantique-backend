604aa951fdde8788cf1decd39914ecce9af64b8a
Dorokhovich Mikhail <mikhail@dorokhovich.ru>
2020-01-02 20:17:17 +0300

    add missed route for categories

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
 
   match 'subscribe', to: 'subscription#subscribe', via: [:post]
   match 'contacts', to: 'contacts#index', via: [:get]
+  match 'categories', to: 'categories#index', via: [:get]
 end
