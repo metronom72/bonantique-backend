1460cb3064e863f4e5ab7435c79e68f34bd0a21d
Dorokhovich Mikhail <mikhail@dorokhovich.ru>
2020-01-12 17:04:35 +0300

    Added route for new bond

--- app/controllers/bonds_controller.rb
+++ app/controllers/bonds_controller.rb
@@ -14,7 +14,48 @@ class BondsController < ApplicationController
     end
   end
 
+  def update
+    @bond = Bond.unscoped.find_by id: params[:id]
+
+    if @bond
+      if @bond.update update_bond_params
+        render json: { data: {
+            bond: @bond,
+            specification: @bond.specification
+        }}
+
+        return
+      else
+        render json: { errors: @bond.errors }, status: 400
+
+        return
+      end
+    else
+      render json: { errors: { api: "Not found" }}, status: 404
+
+      return
+    end
+  end
+
   private
+  def update_bond_params
+    params.require(:bond).permit(
+        :title,
+        :description,
+        :valid_till,
+        :available,
+        :amount,
+        :product_type,
+        :bond_currency,
+        :bond_value,
+        :bond_serial,
+        :bond_number,
+        :is_copy,
+        :quality,
+        prices: [],
+        bond_country: [],
+        )
+  end
   def new_bond_params
     params.require(:new_bond).permit(
       :title,
